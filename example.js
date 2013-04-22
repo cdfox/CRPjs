5a15a4baa9d82a022ff935fca27918e8559824c2
Chris Fox <chrisdavidfox@gmail.com>
2013-04-22 16:08:09 -0400

    added interface to run example and try different parameter values

--- example.js
+++ example.js
@@ -1,5 +1,23 @@
 haskell_tweets = JSON.parse('[["why", "haskell", "is", "worth", "learning", "http://t.co/ssvrxcfhj8"], ["atomic", "spinwhy", "haskell", "is", "worth", "learning", "http://t.co/ssvrxcfhj8"], ["@macasek", "what", "is", "this?", "2008?", "i", "liked", "haskell", "when", "it", "was", "underground.", "now", "it\'s", "too", "mainstream"], ["\\"the", "build", "function\\"", "https://t.co/nre85mmqd6", "--", "a", "good", "explanation", "of", "foldr/build", "fusion", "in", "haskell"], ["@cloudd_o9", "-", "youu", "go", "read", "a", "book", "!"], ["rt", "@cloudd_o9:", "@haskell__", "lol", ".", "go", "read", "a", "book,"], ["rt", "@tacticalgrace:", "complete", "parser", "and", "quasi-quotation", "support", "for", "objective-c", "in", "haskell:", "https://t.co/llqqqjzrjd"], ["@haskell__", "lol", ".", "go", "read", "a", "book,"], ["@seanparsons", "@puffnfresh", "if", "you", "can", "show", "me", "an", "amazing", "game", "done", "with", "haskell,", "i", "will", "renounce", "my", "worldly", "possessions", "and", "switch."], ["rt", "@tacticalgrace:", "complete", "parser", "and", "quasi-quotation", "support", "for", "objective-c", "in", "haskell:", "https://t.co/llqqqjzrjd"], ["youu", "fucking", "hoee", "!"], ["@james_haskell", "we", "just", "gotta", "put", "in", "the", "work", "and", "we\'ll", "be", "in", "a", "good", "spot"], ["@nickcarter", "hope", "to", "see", "you", "and", "the", "rest", "off", "the", "bsb", "boys", "at", "the", "echo", "arena", "in", "liverpool", "please", "can", "i", "have", "a", "follow."], ["cufp", "2013", "call", "for", "presentations", "http://t.co/nulaaxr7ga", "#fsharp", "#haskell", "#scala", "#lisp"], ["rt", "@deech:", "til", "the", "difference", "between", "a", "continuation", "and", "a", "callback", "is", "the", "latter", "is", "some", "kind", "of", "io", "action", "as", "well.", "http://t.co/yo778gnq5g"], ["@d_hart99", "@adrienacosta", "chillen", "with", "haskell", "tryna", "meet", "up?"], ["what", "sucks", "about", "haskell", "is", "that", "i", "can\'t", "find", "a", "fucking", "job.", "and", "bottom.", "yes,", "bottom", "sucks.", "(only", "half", "kidding;", "the", "bottom", "half", "is", "a", "joke.)"], ["rt", "@haskellu:", "the", "second", "haskell", "news", "for", "spring", "2013!!", "student", "produced!", "http://t.co/mxuqbcszr5"], ["rt", "@tacticalgrace:", "complete", "parser", "and", "quasi-quotation", "support", "for", "objective-c", "in", "haskell:", "https://t.co/llqqqjzrjd"], ["rt", "@tacticalgrace:", "complete", "parser", "and", "quasi-quotation", "support", "for", "objective-c", "in", "haskell:", "https://t.co/llqqqjzrjd"], ["rt", "@tsanthoopah:", "@tasunkeluta", "@tksaurus_rex", "nah,", "i", "love", "supporting", "haskell", "clubs........"], ["rt", "@old_sound:", "\\"do", "you", "want", "to", "be", "locked", "into", "tricycles", "because", "they\\u2019re", "easy", "to", "learn?\\"", "http://t.co/vipa7bv0a0", "#haskell"], ["miriam", "haskell", "style", "#bridal", "pearl", "necklace", "by", "amagnificentmess", "http://t.co/xxorvtlncp", "via", "#etsy", "#vjse2", "#teamlove", "#vintage", "#jewelry"], ["rt", "@k0001:", "here", "@jobvranish", "explains", "why", "you", "should", "grow", "up", "and", "use", "haskell", "\\u2192", "http://t.co/5ihmofnymz"], ["complete", "parser", "and", "quasi-quotation", "support", "for", "objective-c", "in", "haskell:", "https://t.co/llqqqjzrjd"], ["rt", "@dyokomizo:", "\\"why", "haskell", "is", "worth", "learning\\"", "http://t.co/pwgqsureak"], ["@puffnfresh", "@seanparsons", "also;", "this", "is", "largely", "true", "for", "haskell,", "but", "i", "bet", "it", "falls", "apart", "as", "soon", "as", "you", "do", "any", "sort", "of", "i/o-based", "monads."], ["here", "@jobvranish", "explains", "why", "you", "should", "grow", "up", "and", "use", "haskell", "\\u2192", "http://t.co/5ihmofnymz"], ["@drmrfrancis", "http://t.co/7fj7efgae9", "switchgrass", "is", "in", "the", "family.", "studied", "in", "ks", "at", "haskell", "univ.", "re:runoff,", "reclamation", "#noviceme"], ["@paniq", "@seanparsons", "i", "live", "in", "the", "purely", "functional", "dream", "world.", "haskell", "and", "scala."], ["@skipaway2000", "would", "your", "dad", "want", "to", "add", "haskell", "hats", "to", "his", "collection?"], ["\\u201c@tartaro_bellaaa:", "@kaykaypaige", "kaitlin", "mother", "fuckin", "haskell.", "your", "fucking", "going\\u201d", "why\\ud83d\\ude12"], ["@kaykaypaige", "kaitlin", "mother", "fuckin", "haskell.", "your", "fucking", "going"], ["soo", "diesel", "tech", "recruiter", "got", "back", "to", "me", "dang", "idk", "what", "to", "do", "haskell", "game", "warden", "wit", "auto", "body", "or", "hastings", "diesel", "@richardkearnes"], ["penalty", "shot", "goal", "to", "start", "it"], ["@sptheghost60", "@adrienacosta", "chillin", "with", "haskell", "hbu", "bro"], ["@puffnfresh", "@seanparsons", "this", "quote", "better", "be", "in", "the", "context", "of", "haskell", "or", "some", "other", "functional", "language."], ["snap", "framework:", "announcing:", "first", "release", "of", "io-streams:", "after", "more", "than", "eight", "months", "of", "careful", "design", "and", "d...", "http://t.co/hoagqhewwk"], ["why", "haskell", "is", "worth", "learning.", "http://t.co/dpgttxrote", "http://t.co/dpgttxrote"], ["@paniq", "haskell", "or", "scala", "most", "likely", "given", "where", "it", "came", "from."], ["rt", "@haskellu:", "the", "second", "haskell", "news", "for", "spring", "2013!!", "student", "produced!", "http://t.co/mxuqbcszr5"], ["why", "are", "the", "sox", "playing", "puerto", "rico", "right", "now?", "they", "def", "aren\'t", "enjoying", "themselves"], ["@cloudd_o9", "-", "lmaoo"], ["rt", "@cloudd_o9:", "@haskell__", "i", "knew", "it", "(:"], ["@nick_russo21", "its", "never", "too", "late", "to", "transfer"], ["4-6", "inches", "may", "kill", "the", "baseball", "grind", "on", "thursday"], ["@haskell__", "i", "knew", "it", "(:"], ["stuck", "with", "my", "eddie", "haskell", "pattern", "since", "high", "school,", "i\'ve", "battled", "it.", "i\'m", "so", "sick", "of", "phone", "etiquette,", "still", "half", "illiterate", "and", "astigmatic"], ["why", "the", "fuck", "is", "this", "game", "blacked", "out", "if", "its", "not", "on", "the", "tv", "espn!", "i\'ve", "missed", "1", "eagles", "game", "all", "year"], ["@cloudd_o9", "-", "exactly", "!"]]');
 
+window.onload = function() {
+	for (var i in haskell_tweets) {
+		var tweet = haskell_tweets[i];
+		var p = document.createElement("p");
+		for (var j in haskell_tweets[i]) {
+			p.innerText += haskell_tweets[i][j] + " ";
+		}
+		p.className = "tweet";
+		document.body.appendChild(p);
+	}
+	document.getElementById("cluster").onclick = function() {
+		var alpha = document.getElementById("alpha").value;
+		var beta = document.getElementById("beta").value;
+		run_example(alpha, beta);
+	};
+}
+
+
 corpus = {}; 
 for (var i in haskell_tweets) {
 	corpus[i] = haskell_tweets[i]
@@ -14,6 +32,7 @@ function show_docs(d) {
 }
 
 function run_example(alpha, beta) {
+	console.log(alpha + " " + beta);
 	if (alpha == undefined) {
 		alpha = 1;
 	}
@@ -40,17 +59,44 @@ function run_example(alpha, beta) {
 		alpha: alpha,
 		beta: beta,
 	}
-	console.log("Clustering " + haskell_tweets.length + " documents (" + state.W + " unique words).");
+	var status_string = "Clustering " + haskell_tweets.length + " documents (" + state.W + " unique words)."
+	console.log(status_string);
 	for (var iter = 1; iter <= 100; iter++) { 
 		for (var docID in corpus) {
 			crpmm.sample_table(docID, corpus, state);
 		}
+		var summary = "";
 		if (iter % 10 == 0) {
 			console.log("ITERATION " + iter + "\n");	
 			for (var tid in state.tables) {
+				summary += "table " + tid + ": num_words " + state.tables[tid].num_words + ", num_docs " + state.tables[tid].num_docs + "\n";
 				console.log("table " + tid + ": num_words " + state.tables[tid].num_words + ", num_docs " + state.tables[tid].num_docs);
 			}
 			console.log("");
 		}
+		document.getElementById("summary").innerText = summary;
+	}
+	tweets = [];
+	for (var docID in corpus) {
+		tweets.push([state.assignments[docID], docID]);
+	}
+	tweets.sort(function(a, b) {
+    	return a[0] - b[0];
+	});
+	var unclustered_tweets = document.getElementsByClassName("tweet");
+	while (unclustered_tweets.length > 0) {
+		unclustered_tweets[0].remove();
+	}
+	for (var i in tweets) {
+		var table = tweets[i][0];
+		var docID = tweets[i][1];
+		var p = document.createElement('p');
+		p.id = "tweet" + i; 
+		p.className = "tweet";
+		p.innerText = "(" + table + ") ";
+		for (var j in haskell_tweets[docID]) {
+			p.innerText += haskell_tweets[docID][j] + " ";
+		}
+		document.body.appendChild(p);
 	}
 }
